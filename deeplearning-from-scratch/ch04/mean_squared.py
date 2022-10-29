52ce50dfe087e345540b410095c32c6fbbd7870c
dorayaki <ytoriyama@gmail.com>
2022-10-29 19:09:40 +0900

    Changes

--- deeplearning-from-scratch/ch04/mean_squared.py
+++ deeplearning-from-scratch/ch04/mean_squared.py
@@ -6,8 +6,10 @@ t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t) ** 2)
 
-
+# 正解が近いバージョン
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 print(mean_squared_error(np.array(y), np.array(t)))
+
+# 正解が遠いバージョン
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 print(mean_squared_error(np.array(y), np.array(t)))
